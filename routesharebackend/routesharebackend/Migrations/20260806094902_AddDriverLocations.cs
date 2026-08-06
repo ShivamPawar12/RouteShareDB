2fe6d42800b068e980bc46dfe261f1b3b6a0e648
ShivamPawar12 <shivampawarsp9@gmail.com>
2026-08-06 16:42:12 +0530

    New Changes

---
+++ routesharebackend/routesharebackend/Migrations/20260806094902_AddDriverLocations.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace routesharebackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDriverLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RideStatus",
+                table: "OfferPool",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DriverId",
+                table: "DriverLocations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RideStatus",
+                table: "OfferPool",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DriverId",
+                table: "DriverLocations",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
